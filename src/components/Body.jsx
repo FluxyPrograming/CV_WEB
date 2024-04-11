6444b9a3ffa3c731569c94ed6c6828999765fba1
lukab <luka.blazic@gmail.com>
2024-04-11 12:47:48 +0200

    added gsap

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,12 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
 export const Body = () => {
   return (
     <>
-      <section className="bg-main-bg">
+      <section className="bg-main-bg text-main-text">
         <p>Hello, World!</p>
       </section>
     </>
