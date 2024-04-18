20bc568332d902c4651698eeeeb1dec28ec1eac1
lukab <luka.blazic@gmail.com>
2024-04-18 13:41:03 +0200

    Added new fonts

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,8 +7,12 @@ export const Body = () => {
   return (
     <>
       <section className="bg-main-bg text-main-text min-h-dvh z-0">
-        <div className="h-20"></div>
-        <div className="m-auto bg-primary h-20 w-3/5 rounded-xl translate-y-32 scale-y-150"></div>
+        <div id="" className="m-auto bg-primary h-20 w-3/5 rounded-xl"></div>
+        <article>
+          <h1 className="text-primary text-8xl m-auto w-fit text-6xl font-medium font-general">
+            - MY PROFILE -
+          </h1>
+        </article>
       </section>
     </>
   );
