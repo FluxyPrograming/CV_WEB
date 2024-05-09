0b6d4f2b6e01905886e0bb134194b4e8bedb2305
lukab <luka.blazic@gmail.com>
2024-05-09 13:47:15 +0200

    Sectionized the body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,18 +1,31 @@
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
+import { EnterAnimation } from "./EnterAnimation";
+import { MyProfile } from "./MyProfile";
+import { Technologies } from "./Technologies";
 
 gsap.registerPlugin(useGSAP);
 
 export const Body = () => {
   return (
     <>
-      <section className="bg-main-bg text-main-text min-h-dvh z-0">
-        <div id="" className="m-auto bg-primary h-20 w-3/5 rounded-xl"></div>
-        <article>
-          <h1 className="text-primary text-8xl m-auto w-fit text-6xl font-medium font-general">
-            - MY PROFILE -
-          </h1>
-        </article>
+      <section
+        id="entrance"
+        className="bg-main-bg text-main-text min-h-dvh z-0 flex flex-col"
+      >
+        <EnterAnimation />
+      </section>
+      <section
+        id="myProfile"
+        className="bg-main-bg text-main-text min-h-dvh z-0 flex flex-col"
+      >
+        <MyProfile />
+      </section>
+      <section
+        id="experience"
+        className="bg-main-bg text-main-text min-h-dvh z-0 flex flex-col"
+      >
+        <Technologies />
       </section>
     </>
   );
