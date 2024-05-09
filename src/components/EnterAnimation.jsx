f70f32dbbf2626c3ba6da8f5ec449161467f1008
lukab <luka.blazic@gmail.com>
2024-05-09 12:37:50 +0200

    Animation element created

---
+++ src/components/EnterAnimation.jsx
@@ -0,0 +1,12 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
+export const EnterAnimation = () => {
+  return (
+    <>
+      <div id="" className="m-auto bg-primary h-40 w-3/5 rounded-xl"></div>
+    </>
+  );
+};
