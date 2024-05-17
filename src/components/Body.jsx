2b654320264bab38ef91d55279340ec498c9a7a8
lukab <luka.blazic@gmail.com>
2024-05-17 09:32:52 +0200

    Added porject and contact components

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,6 +3,8 @@ import { useGSAP } from "@gsap/react";
 import { EnterAnimation } from "./EnterAnimation";
 import { MyProfile } from "./MyProfile";
 import { Technologies } from "./Technologies";
+import { Projects } from "./Projects";
+import { ContactMe } from "./ContactMe";
 
 gsap.registerPlugin(useGSAP);
 
@@ -26,6 +28,18 @@ export const Body = () => {
         className="bg-main-bg text-main-text min-h-dvh z-0 flex flex-col"
       >
         <Technologies />
+      </section>
+      <section
+        id="projects"
+        className="bg-main-bg text-main-text min-h-dvh z-0 flex flex-col"
+      >
+        <Projects />
+      </section>
+      <section
+        id="contact"
+        className="bg-main-bg text-main-text min-h-dvh z-0 flex flex-col"
+      >
+        <ContactMe />
       </section>
     </>
   );
