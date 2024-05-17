d827f0680af7194e5a671a05abcf08d8058f3057
lukab <luka.blazic@gmail.com>
2024-05-17 09:32:06 +0200

    Added headings

---
+++ src/components/ContactMe.jsx
@@ -0,0 +1,11 @@
+export const ContactMe = () => {
+  return (
+    <>
+      <article>
+        <h1 className="text-primary text-8xl mt-20 ml-auto mr-auto w-fit text-6xl font-medium font-general">
+          - CONTACT -
+        </h1>
+      </article>
+    </>
+  );
+};
---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+export const Projects = () => {
+  return (
+    <>
+      <article>
+        <h1 className="text-primary text-8xl mt-20 ml-auto mr-auto w-fit text-6xl font-medium font-general">
+          - SELECTED WORKS -
+        </h1>
+      </article>
+    </>
+  );
+};
