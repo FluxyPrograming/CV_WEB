e7979d8afb82880513502f7179bf55518c5bfd69
lukab <luka.blazic@gmail.com>
2024-05-20 08:00:00 +0200

    Using the custom hook for the animation

--- src/components/EnterAnimation.jsx
+++ src/components/EnterAnimation.jsx
@@ -1,12 +1,31 @@
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
+import { useIntersection } from "../hooks/useIntersection";
+import { useRef, useEffect } from "react";
 
 gsap.registerPlugin(useGSAP);
 
+const enterAnimation = () => {
+  const t1 = gsap.timeline({ repeat: 0 });
+  t1.to("#enter", { scaleY: 4 });
+};
+
 export const EnterAnimation = () => {
+  const triggerRef = useRef(null);
+  const isVisible = useIntersection(triggerRef, "0px");
+
+  useEffect(() => {
+    if (isVisible) {
+      enterAnimation();
+    }
+  }, [enterAnimation, isVisible]);
   return (
     <>
-      <div id="" className="m-auto bg-primary h-40 w-3/5 rounded-xl"></div>
+      <div
+        ref={triggerRef}
+        id="enter"
+        className="m-auto bg-primary h-40 w-3/5 rounded-xl"
+      ></div>
     </>
   );
 };
