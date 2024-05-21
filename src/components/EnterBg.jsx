c60c26c44786fdfabc06e19ccc4037c4d794b7ea
lukab <luka.blazic@gmail.com>
2024-05-21 10:30:05 +0200

    Added Bg svg for enter animation

---
+++ src/components/EnterBg.jsx
@@ -0,0 +1,254 @@
+export const EnterBg = () => {
+  return (
+    <>
+      <svg
+        id="visual"
+        viewBox="0 0 600 900"
+        width="600"
+        height="900"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        version="1.1"
+      >
+        <g stroke-width="1" stroke-linejoin="bevel">
+          <path
+            d="M328 439L417 408L413 320Z"
+            fill="#a4d0ca"
+            stroke="#a4d0ca"
+          ></path>
+          <path
+            d="M413 320L328 291L328 439Z"
+            fill="#337067"
+            stroke="#337067"
+          ></path>
+          <path
+            d="M328 291L169 438L328 439Z"
+            fill="#4a827a"
+            stroke="#4a827a"
+          ></path>
+          <path
+            d="M169 438L297 562L328 439Z"
+            fill="#8dbcb5"
+            stroke="#8dbcb5"
+          ></path>
+          <path
+            d="M328 439L408 623L417 408Z"
+            fill="#6b9f97"
+            stroke="#6b9f97"
+          ></path>
+          <path
+            d="M413 320L449 174L328 291Z"
+            fill="#3e7970"
+            stroke="#3e7970"
+          ></path>
+          <path
+            d="M328 291L107 259L169 438Z"
+            fill="#76a9a1"
+            stroke="#76a9a1"
+          ></path>
+          <path
+            d="M600 306L449 174L413 320Z"
+            fill="#558c83"
+            stroke="#558c83"
+          ></path>
+          <path
+            d="M449 174L291 132L328 291Z"
+            fill="#76a9a1"
+            stroke="#76a9a1"
+          ></path>
+          <path
+            d="M600 567L600 423L417 408Z"
+            fill="#337067"
+            stroke="#337067"
+          ></path>
+          <path
+            d="M417 408L600 306L413 320Z"
+            fill="#558c83"
+            stroke="#558c83"
+          ></path>
+          <path
+            d="M600 423L600 306L417 408Z"
+            fill="#8dbcb5"
+            stroke="#8dbcb5"
+          ></path>
+          <path
+            d="M297 562L408 623L328 439Z"
+            fill="#558c83"
+            stroke="#558c83"
+          ></path>
+          <path
+            d="M291 132L107 259L328 291Z"
+            fill="#a4d0ca"
+            stroke="#a4d0ca"
+          ></path>
+          <path
+            d="M0 408L151 579L169 438Z"
+            fill="#a4d0ca"
+            stroke="#a4d0ca"
+          ></path>
+          <path
+            d="M169 438L151 579L297 562Z"
+            fill="#76a9a1"
+            stroke="#76a9a1"
+          ></path>
+          <path
+            d="M297 562L312 741L408 623Z"
+            fill="#8dbcb5"
+            stroke="#8dbcb5"
+          ></path>
+          <path
+            d="M600 306L600 173L449 174Z"
+            fill="#6b9f97"
+            stroke="#6b9f97"
+          ></path>
+          <path
+            d="M422 0L322 0L291 132Z"
+            fill="#98c6c0"
+            stroke="#98c6c0"
+          ></path>
+          <path
+            d="M408 623L600 567L417 408Z"
+            fill="#60958d"
+            stroke="#60958d"
+          ></path>
+          <path
+            d="M291 132L107 164L107 259Z"
+            fill="#76a9a1"
+            stroke="#76a9a1"
+          ></path>
+          <path
+            d="M107 259L0 408L169 438Z"
+            fill="#6b9f97"
+            stroke="#6b9f97"
+          ></path>
+          <path
+            d="M0 310L0 408L107 259Z"
+            fill="#76a9a1"
+            stroke="#76a9a1"
+          ></path>
+          <path
+            d="M107 164L0 310L107 259Z"
+            fill="#98c6c0"
+            stroke="#98c6c0"
+          ></path>
+          <path
+            d="M422 0L291 132L449 174Z"
+            fill="#3e7970"
+            stroke="#3e7970"
+          ></path>
+          <path
+            d="M291 132L163 0L107 164Z"
+            fill="#60958d"
+            stroke="#60958d"
+          ></path>
+          <path
+            d="M600 0L422 0L449 174Z"
+            fill="#82b2ab"
+            stroke="#82b2ab"
+          ></path>
+          <path
+            d="M176 711L312 741L297 562Z"
+            fill="#8dbcb5"
+            stroke="#8dbcb5"
+          ></path>
+          <path
+            d="M408 623L469 760L600 567Z"
+            fill="#4a827a"
+            stroke="#4a827a"
+          ></path>
+          <path
+            d="M0 594L176 711L151 579Z"
+            fill="#558c83"
+            stroke="#558c83"
+          ></path>
+          <path
+            d="M151 579L176 711L297 562Z"
+            fill="#3e7970"
+            stroke="#3e7970"
+          ></path>
+          <path
+            d="M312 741L469 760L408 623Z"
+            fill="#6b9f97"
+            stroke="#6b9f97"
+          ></path>
+          <path
+            d="M322 0L163 0L291 132Z"
+            fill="#4a827a"
+            stroke="#4a827a"
+          ></path>
+          <path
+            d="M107 164L0 110L0 310Z"
+            fill="#6b9f97"
+            stroke="#6b9f97"
+          ></path>
+          <path
+            d="M0 408L0 594L151 579Z"
+            fill="#558c83"
+            stroke="#558c83"
+          ></path>
+          <path
+            d="M163 0L0 110L107 164Z"
+            fill="#4a827a"
+            stroke="#4a827a"
+          ></path>
+          <path
+            d="M600 173L600 0L449 174Z"
+            fill="#337067"
+            stroke="#337067"
+          ></path>
+          <path
+            d="M469 760L600 772L600 567Z"
+            fill="#82b2ab"
+            stroke="#82b2ab"
+          ></path>
+          <path d="M163 0L0 0L0 110Z" fill="#3e7970" stroke="#3e7970"></path>
+          <path
+            d="M0 594L0 766L176 711Z"
+            fill="#98c6c0"
+            stroke="#98c6c0"
+          ></path>
+          <path
+            d="M176 711L278 900L312 741Z"
+            fill="#4a827a"
+            stroke="#4a827a"
+          ></path>
+          <path
+            d="M312 741L454 900L469 760Z"
+            fill="#8dbcb5"
+            stroke="#8dbcb5"
+          ></path>
+          <path
+            d="M469 760L600 900L600 772Z"
+            fill="#8dbcb5"
+            stroke="#8dbcb5"
+          ></path>
+          <path
+            d="M125 900L278 900L176 711Z"
+            fill="#6b9f97"
+            stroke="#6b9f97"
+          ></path>
+          <path
+            d="M278 900L454 900L312 741Z"
+            fill="#558c83"
+            stroke="#558c83"
+          ></path>
+          <path
+            d="M0 766L125 900L176 711Z"
+            fill="#3e7970"
+            stroke="#3e7970"
+          ></path>
+          <path
+            d="M454 900L600 900L469 760Z"
+            fill="#82b2ab"
+            stroke="#82b2ab"
+          ></path>
+          <path
+            d="M0 766L0 900L125 900Z"
+            fill="#82b2ab"
+            stroke="#82b2ab"
+          ></path>
+        </g>
+      </svg>
+    </>
+  );
+};
