a5b1917a99d529d342578e6928ff04eb65f29202
lukab <luka.blazic@gmail.com>
2024-05-21 09:56:25 +0200

    New branch

--- src/components/EnterAnimation.jsx
+++ src/components/EnterAnimation.jsx
@@ -1,7 +1,5 @@
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import { useIntersection } from "../hooks/useIntersection";
-import { useRef, useEffect } from "react";
 
 gsap.registerPlugin(useGSAP);
 
@@ -11,21 +9,9 @@ const enterAnimation = () => {
 };
 
 export const EnterAnimation = () => {
-  const triggerRef = useRef(null);
-  const isVisible = useIntersection(triggerRef, "0px");
-
-  useEffect(() => {
-    if (isVisible) {
-      enterAnimation();
-    }
-  }, [enterAnimation, isVisible]);
   return (
     <>
-      <div
-        ref={triggerRef}
-        id="enter"
-        className="m-auto bg-primary h-40 w-3/5 rounded-xl"
-      ></div>
+      <div id="enter" className="m-auto bg-primary h-40 w-3/5 rounded-xl"></div>
     </>
   );
 };
