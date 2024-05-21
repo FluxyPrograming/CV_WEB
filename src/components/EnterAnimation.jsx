b1619dabcb7175f7990d2f23a785311f69ff1d3d
lukab <luka.blazic@gmail.com>
2024-05-21 10:30:32 +0200

    Working on timeline for the animation

--- src/components/EnterAnimation.jsx
+++ src/components/EnterAnimation.jsx
@@ -1,14 +1,14 @@
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(useGSAP);
-
-const enterAnimation = () => {
-  const t1 = gsap.timeline({ repeat: 0 });
-  t1.to("#enter", { scaleY: 4 });
-};
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export const EnterAnimation = () => {
+  let t1 = gsap.timeline({
+    scrollTrigger: { trigger: "#enter", start: "top top", end: "+=500" },
+  });
+
   return (
     <>
       <div id="enter" className="m-auto bg-primary h-40 w-3/5 rounded-xl"></div>
