63444e005d7314c5e77c7404bcabfc1842df7026
Luke <luka.blazic666@gmail.com>
2024-06-01 11:32:35 +0200

    Added Lenis smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { ReactLenis, useLenis } from "lenis/react";
 import { Header } from "./components/Header.jsx";
 import { Body } from "./components/Body.jsx";
 
 function App() {
+  const lenis = useLenis(({ scroll }) => {
+    // called every scroll
+  });
   return (
-    <>
+    <ReactLenis root>
       <Header />
       <Body />
-    </>
+    </ReactLenis>
   );
 }
 
