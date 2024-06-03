c5be16d899863baf9e5ed405a737089979abb692
Luke <luka.blazic666@gmail.com>
2024-06-03 19:57:36 +0200

    Added poly bg

--- src/components/EnterAnimation.jsx
+++ src/components/EnterAnimation.jsx
@@ -9,14 +9,27 @@ export const EnterAnimation = () => {
   tl.to("#enter", {
     scale: 1.8,
     duration: 1.5,
+  }).to(".tt", {
+    scale: 1.4,
+    duration: 1.2,
   });
 
   return (
     <>
       <div id="enter" className="m-auto font-general font-bold text-6xl">
-        <p className="block">HI, I'M LUKA</p>
-        <p className="block text-main-bg">HI, I'M LUKA</p>
-        <p className="block">HI, I'M LUKA</p>
+        <img
+          src="src\assets\low-poly-grid-haikei.svg"
+          alt=""
+          className="bg absolute  h-[600px] w-[500px] top-[-10%] left-[5%] z-[-1]"
+        />
+        <p className="tt block z-10 m-[20px]">Hi, I'm Luka</p>
+        <p
+          className="tt block z-10 m-[20px] text-[transparent]"
+          style={{ WebkitTextStroke: "2px white" }}
+        >
+          Hi, I'm Luka
+        </p>
+        <p className="tt block z-10 m-[20px]">Hi, I'm Luka</p>
       </div>
     </>
   );
