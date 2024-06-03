db36ad9c8b0eb21c6ea71708cbc6ad8fea4578ba
Luke <luka.blazic666@gmail.com>
2024-06-03 20:41:12 +0200

    Added "WIP"

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,9 @@ export const Projects = () => {
         <h1 className="text-primary text-8xl mt-20 ml-auto mr-auto w-fit text-6xl font-medium font-general">
           - SELECTED WORKS -
         </h1>
+        <div className="m-auto text-[300px] font-grotesk w-fit mt-[200px]">
+          WIP
+        </div>
       </article>
     </>
   );
